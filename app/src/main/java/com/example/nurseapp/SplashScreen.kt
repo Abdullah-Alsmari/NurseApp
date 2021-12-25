bc8a2db60b10580975bb7a1cc30e95564714f839
A <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-25 21:37:09 +0530

    added splash screen

---
+++ app/src/main/java/com/example/nurseapp/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.example.nurseapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.abdullah.nurseapp.DashboardActivity
+import com.abdullah.nurseapp.LoginActivity
+import com.abdullah.nurseapp.R
+import com.example.nurseapp.utils.ISLOGGEDIN
+import com.example.nurseapp.utils.getBooleanFromPrefs
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_splash_screen)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            if (getBooleanFromPrefs(this, ISLOGGEDIN)) {
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
