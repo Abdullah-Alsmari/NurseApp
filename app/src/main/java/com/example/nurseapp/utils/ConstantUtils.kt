9d4ea2ccf9345f100e636e46d3011de87d508eae
A <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-25 21:37:27 +0530

    added shared pref util

---
+++ app/src/main/java/com/example/nurseapp/utils/ConstantUtils.kt
@@ -0,0 +1,8 @@
+package com.example.nurseapp.utils
+
+
+//login constants
+const val EMAIL = "email"
+const val USERNAME = "name"
+const val PHONENUMBER = "phonenumber"
+const val ISLOGGEDIN = "loggedin"
