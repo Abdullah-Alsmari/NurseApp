36b93f2529b2437883bfd62ba30679a220767504
A <Abdullah.Mohammed.ruh.k@tuwaiq.edu.sa>
2022-01-14 01:06:34 +0530

    added scroll view in home and add task fragment

--- app/src/main/java/com/abdullah/nurseapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/abdullah/nurseapp/fragment/HomeFragment.kt
@@ -51,6 +51,8 @@ class HomeFragment : Fragment(){
         val list = mutableListOf<HomeModel>()
         list.add(HomeModel("X-ray", R.drawable.xrayicon))
         list.add(HomeModel("Blood Testing",R.drawable.bloodtestingicon))
+        list.add(HomeModel("Blood Testing",R.drawable.bloodtestingicon))
+        list.add(HomeModel("Blood Testing",R.drawable.bloodtestingicon))
         return list
 
     }
